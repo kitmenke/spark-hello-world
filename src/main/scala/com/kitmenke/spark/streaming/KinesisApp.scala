50ab2b5d17d0d88db8a2bf8e799332f9da809f3b
Kit Menke <cwmenke@gmail.com>
2019-04-02 11:07:11 -0500

    Parse json message.

--- src/main/scala/com/kitmenke/spark/streaming/KinesisApp.scala
+++ src/main/scala/com/kitmenke/spark/streaming/KinesisApp.scala
@@ -6,8 +6,10 @@ import com.amazonaws.services.kinesis.clientlibrary.lib.worker.InitialPositionIn
 import org.apache.log4j.Logger
 import org.apache.solr.client.solrj.impl.ConcurrentUpdateSolrClient
 import org.apache.solr.common.SolrInputDocument
-import org.apache.spark.SparkConf
+import org.apache.spark.{SparkConf, sql}
 import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
+import org.apache.spark.sql.{DataFrame, Row, SQLContext, SparkSession}
 import org.apache.spark.storage.StorageLevel
 import org.apache.spark.streaming.kinesis.KinesisInputDStream
 import org.apache.spark.streaming.{Seconds, StreamingContext}
@@ -17,10 +19,11 @@ object KinesisApp {
   val jobName = "KinesisApp"
 
   def main(args: Array[String]): Unit = {
+    // Create a local spark session and streaming context
     val master = "local[*]"
     val interval = Seconds(5)
-    val conf = new SparkConf().setAppName(jobName).setMaster(master)
-    val ssc = new StreamingContext(conf, interval)
+    val sparkSession = SparkSession.builder().appName(jobName).master(master).getOrCreate()
+    val ssc = new StreamingContext(sparkSession.sparkContext, interval)
 
     // Note: You must setup your AWS Credentials before this will work
     // See http://docs.aws.amazon.com/AWSSdkDocsJava/latest/DeveloperGuide/credentials.html
@@ -29,30 +32,52 @@ object KinesisApp {
       .endpointUrl("https://kinesis.us-east-1.amazonaws.com")
       .regionName("us-east-1")
       .streamName("DE_April_Meetup_KS0")
-      .initialPositionInStream(InitialPositionInStream.LATEST)
       .checkpointAppName(jobName)
       .checkpointInterval(interval)
       .storageLevel(StorageLevel.MEMORY_AND_DISK_2)
       .build()
-    kinesisStream.foreachRDD(rdd => writeRddToSolr(rdd))
+    kinesisStream.foreachRDD(rdd => writeRddToSolr(sparkSession, rdd))
     ssc.start()
     ssc.awaitTermination()
   }
 
-  def writeRddToSolr(rdd: RDD[Array[Byte]]): Unit = {
-
-    rdd.foreachPartition(iter => {
+  def writeRddToSolr(spark: SparkSession, rdd: RDD[Array[Byte]]): Unit = {
+    val rows: RDD[String] = rdd.map(bytes => new String(bytes))
+    val df = jsonToDataframe(spark, rows)
+    df.foreachPartition(iter => {
       val connection = new ConcurrentUpdateSolrClient.Builder("http://localhost:8983/solr/gettingstarted").build()
-      iter.foreach(bytes => {
-        val value = new String(bytes)
-        val doc = new SolrInputDocument()
-        doc.setField("id", UUID.randomUUID().toString)
-        doc.setField("message", value)
-        doc.setField("created", new Date())
-        connection.add(doc)
+      iter.foreach(row => {
+        connection.add(rowToSolrDocument(row))
       })
       connection.commit()
       connection.close()
     })
+  }
+
+  val twilioJsonSchema: StructType = StructType(List(
+    StructField("FromZip", StringType, nullable = true),
+    StructField("FromCity", StringType, nullable = true),
+    StructField("FromState", StringType, nullable = true),
+    StructField("Body", StringType, nullable = true)
+  ))
+
+  def jsonToDataframe(sparkSession: SparkSession, rdd: RDD[String]): DataFrame = {
+    val rows = rdd.map(s => sql.Row(s))
+    val schema = StructType(Seq(StructField("twilio_json", StringType, nullable = true)))
+    val df = sparkSession.createDataFrame(rows, schema)
+    val df2 = df.select(sql.functions.from_json(df("twilio_json"), twilioJsonSchema).as("parsed_json"))
+    df2.select("parsed_json.*")
+  }
+
+  def rowToSolrDocument(row: Row): SolrInputDocument = {
+    val doc = new SolrInputDocument()
+    doc.setField("id", UUID.randomUUID().toString)
+    doc.setField("Body", row.getAs[String]("Body"))
+    doc.setField("FromZip", row.getAs[String]("FromZip"))
+    doc.setField("FromCity", row.getAs[String]("FromCity"))
+    doc.setField("FromState", row.getAs[String]("FromState"))
+    doc.setField("sentiment", "Neutral")
+    doc.setField("created", new Date())
+    doc
   }
 }
---
+++ src/test/scala/com/kitmenke/spark/streaming/KinesisAppTests.scala
@@ -0,0 +1,29 @@
+package com.kitmenke.spark.streaming
+
+import com.holdenkarau.spark.testing.DataFrameSuiteBase
+import org.apache.spark.sql
+import org.scalatest.FunSpec
+
+class KinesisAppTests extends FunSpec with DataFrameSuiteBase {
+  describe("Kinesis App Tests") {
+    it("should be able to parse json into a dataframe") {
+      // Example data to parse
+      val data = List(
+        "{\"ToCountry\": \"US\", \"ToState\": \"MO\", \"SmsMessageSid\": \"SM52c2a4d44e5490eb9766fabd9e6734d6\", \"NumMedia\": \"0\", \"ToCity\": \"\", \"FromZip\": \"62966\", \"SmsSid\": \"SM52c2a4d44e5490eb9766fabd9e6734d6\", \"FromState\": \"IL\", \"SmsStatus\": \"received\", \"FromCity\": \"MURPHYSBORO\", \"Body\": \"Hello there\", \"FromCountry\": \"US\", \"To\": \"+13143105899\", \"ToZip\": \"\", \"NumSegments\": \"1\", \"MessageSid\": \"SM52c2a4d44e5490eb9766fabd9e6734d6\", \"AccountSid\": \"AC0262aa9f4698799c46277c96da98dbb0\", \"From\": \"+13145551212\", \"ApiVersion\": \"2010-04-01\", \"insertdt\": \"2019-04-01 15:41:35\"}",
+        "{\"ToCountry\": \"US\", \"ToState\": \"MO\", \"SmsMessageSid\": \"SMc453b52badcb09451836d3fcb944cbfa\", \"NumMedia\": \"0\", \"ToCity\": \"\", \"FromZip\": \"20166\", \"SmsSid\": \"SMc453b52badcb09451836d3fcb944cbfa\", \"FromState\": \"VA\", \"SmsStatus\": \"received\", \"FromCity\": \"STERLING\", \"Body\": \"SMS from Python\", \"FromCountry\": \"US\", \"To\": \"+13143105899\", \"ToZip\": \"\", \"NumSegments\": \"1\", \"MessageSid\": \"SMc453b52badcb09451836d3fcb944cbfa\", \"AccountSid\": \"AC0262aa9f4698799c46277c96da98dbb0\", \"From\": \"+13145551212\", \"ApiVersion\": \"2010-04-01\", \"insertdt\": \"2019-04-01 15:38:55\"}",
+        "{\"ToCountry\": \"US\", \"ToState\": \"MO\", \"SmsMessageSid\": \"SM9b78712388f32a36ed9a8143f9afb391\", \"NumMedia\": \"0\", \"ToCity\": \"\", \"FromZip\": \"20166\", \"SmsSid\": \"SM9b78712388f32a36ed9a8143f9afb391\", \"FromState\": \"VA\", \"SmsStatus\": \"received\", \"FromCity\": \"STERLING\", \"Body\": \"How are you?\", \"FromCountry\": \"US\", \"To\": \"+13143105899\", \"ToZip\": \"\", \"NumSegments\": \"1\", \"MessageSid\": \"SM9b78712388f32a36ed9a8143f9afb391\", \"AccountSid\": \"AC0262aa9f4698799c46277c96da98dbb0\", \"From\": \"+13145551212\", \"ApiVersion\": \"2010-04-01\", \"insertdt\": \"2019-04-01 15:38:51\"}"
+      )
+      val rdd = sc.parallelize(data)
+      val expectedData = List(
+        sql.Row("62966", "MURPHYSBORO", "IL", "Hello there"),
+        sql.Row("20166", "STERLING", "VA", "SMS from Python"),
+        sql.Row("20166", "STERLING", "VA", "How are you?")
+      )
+
+      val expectedDf = spark.createDataFrame(sc.parallelize(expectedData), KinesisApp.twilioJsonSchema)
+      val actualDf = KinesisApp.jsonToDataframe(spark, rdd)
+      assertDataFrameEquals(expectedDf, actualDf)
+    }
+  }
+
+}
