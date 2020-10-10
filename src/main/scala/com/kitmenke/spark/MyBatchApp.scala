3c61734a29b53c2baa5b2e82dbed14cc2c9e4ef7
Tim Sagona <tsagona@1904labs.com>
2020-10-10 16:59:07 -0500

    Initial solution

--- src/main/scala/com/kitmenke/spark/MyBatchApp.scala
+++ src/main/scala/com/kitmenke/spark/MyBatchApp.scala
@@ -2,30 +2,23 @@ package com.kitmenke.spark
 
 import org.apache.log4j.Logger
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.col
 
-object MyApp {
+object MyBatchApp {
   lazy val logger: Logger = Logger.getLogger(this.getClass)
   val jobName = "MyApp"
 
   def main(args: Array[String]): Unit = {
-    val sentences = Seq(
-      "Space.",
-      "The final frontier.",
-      "These are the voyages of the starship Enterprise.",
-      "Its continuing mission:",
-      "to explore strange new worlds,",
-      "to seek out new life and new civilizations,",
-      "to boldly go where no one has gone before!"
-    )
 
     try {
       val spark = SparkSession.builder().appName(jobName).master("local[1]").getOrCreate()
-      val rdd = spark.sparkContext.parallelize(sentences)
-      val counts = rdd.flatMap(splitSentenceIntoWords)
+      import spark.implicits._
+      val sentences = spark.read.csv("src/main/resources/sentences.txt").as[String]
+      val counts = sentences.flatMap(splitSentenceIntoWords)
         .map(word => (word, 1))
-        .reduceByKey(_ + _)
-        .sortBy(t => t._2, ascending = false)
-      counts.foreach(println)
+        .groupByKey(_._1).count()
+      counts.printSchema()
+      counts.sort(col("count(1)").desc).foreach(println(_))
     } catch {
       case e: Exception => logger.error(s"$jobName error in main", e)
     }
--- src/main/scala/com/kitmenke/spark/MyStreamingApp.scala
+++ src/main/scala/com/kitmenke/spark/MyStreamingApp.scala
@@ -4,7 +4,7 @@ import org.apache.log4j.Logger
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.streaming.{OutputMode, Trigger}
 import org.apache.spark.sql.types.{IntegerType, StringType, StructType, TimestampType}
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
 
 /**
  * Spark Structured Streaming app
@@ -15,28 +15,31 @@ object MyStreamingApp {
   lazy val logger: Logger = Logger.getLogger(this.getClass)
   val jobName = "MyStreamingApp"
   // TODO: define the schema for parsing data from Kafka
-  val schema: StructType = ???
 
   def main(args: Array[String]): Unit = {
     try {
       val spark = SparkSession.builder().appName(jobName).master("local[*]").getOrCreate()
+      import spark.implicits._
+
       val bootstrapServers = args(0)
-      val df = spark
+      val sentences = spark
         .readStream
         .format("kafka")
         .option("kafka.bootstrap.servers", bootstrapServers)
-        .option("subscribe", "reviews")
+        .option("subscribe", "word-count")
         .load()
-        .selectExpr("CAST(value AS STRING)")
+        .selectExpr("CAST(value AS STRING)").as[String]
 
-      df.printSchema()
+      val counts = sentences.flatMap(splitSentenceIntoWords)
+        .map(word => (word, 1))
+        .groupByKey(_._1).count()
 
-      val out = compute(df)
 
-      val query = out.writeStream
+
+      val query = counts.writeStream
         .outputMode(OutputMode.Complete())
         .format("console")
-        .trigger(Trigger.ProcessingTime("5 seconds"))
+        .trigger(Trigger.ProcessingTime("30 seconds"))
         .start()
 
       query.awaitTermination()
@@ -44,9 +47,8 @@ object MyStreamingApp {
       case e: Exception => logger.error(s"$jobName error in main", e)
     }
   }
-
-  def compute(df: DataFrame): DataFrame = {
-    // TODO: implement your logic here
-    df
+  def splitSentenceIntoWords(sentence: String): Array[String] = {
+    sentence.split(" ").map(word => word.toLowerCase.replaceAll("[^a-z]", ""))
   }
+
 }
