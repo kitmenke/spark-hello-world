2b75a8c09a9dfa5a15cec6ffd2edab15c08f2bff
Tim Sagona <tsagona@1904labs.com>
2020-10-11 12:05:06 -0500

    Adding spark.sql.shuffle.partitions

--- src/main/scala/com/kitmenke/spark/MyBatchApp.scala
+++ src/main/scala/com/kitmenke/spark/MyBatchApp.scala
@@ -2,7 +2,7 @@ package com.kitmenke.spark
 
 import org.apache.log4j.Logger
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.functions._
 
 object MyBatchApp {
   lazy val logger: Logger = Logger.getLogger(this.getClass)
@@ -11,14 +11,19 @@ object MyBatchApp {
   def main(args: Array[String]): Unit = {
 
     try {
-      val spark = SparkSession.builder().appName(jobName).master("local[1]").getOrCreate()
+      val spark = SparkSession.builder()
+        .appName(jobName)
+        .config("spark.sql.shuffle.partitions", "3")
+        .master("local[*]")
+        .getOrCreate()
       import spark.implicits._
+
       val sentences = spark.read.csv("src/main/resources/sentences.txt").as[String]
+
       val counts = sentences.flatMap(splitSentenceIntoWords)
-        .map(word => (word, 1))
-        .groupByKey(_._1).count()
-      counts.printSchema()
-      counts.sort(col("count(1)").desc).foreach(println(_))
+        .groupBy("value").count().sort(desc("count"))
+
+      counts.foreach(println(_))
     } catch {
       case e: Exception => logger.error(s"$jobName error in main", e)
     }
--- src/main/scala/com/kitmenke/spark/MyStreamingApp.scala
+++ src/main/scala/com/kitmenke/spark/MyStreamingApp.scala
@@ -3,8 +3,7 @@ package com.kitmenke.spark
 import org.apache.log4j.Logger
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.streaming.{OutputMode, Trigger}
-import org.apache.spark.sql.types.{IntegerType, StringType, StructType, TimestampType}
-import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
+import org.apache.spark.sql.SparkSession
 
 /**
  * Spark Structured Streaming app
@@ -18,10 +17,16 @@ object MyStreamingApp {
 
   def main(args: Array[String]): Unit = {
     try {
-      val spark = SparkSession.builder().appName(jobName).master("local[*]").getOrCreate()
+
+      val spark = SparkSession.builder()
+        .appName(jobName)
+        .config("spark.sql.shuffle.partitions", "3")
+        .master("local[*]")
+        .getOrCreate()
       import spark.implicits._
 
       val bootstrapServers = args(0)
+
       val sentences = spark
         .readStream
         .format("kafka")
@@ -31,10 +36,7 @@ object MyStreamingApp {
         .selectExpr("CAST(value AS STRING)").as[String]
 
       val counts = sentences.flatMap(splitSentenceIntoWords)
-        .map(word => (word, 1))
-        .groupByKey(_._1).count()
-
-
+        .groupBy("value").count().sort(desc("count"))
 
       val query = counts.writeStream
         .outputMode(OutputMode.Complete())
