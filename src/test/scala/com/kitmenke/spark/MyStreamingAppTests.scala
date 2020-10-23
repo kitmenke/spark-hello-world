678e9af29e7703a51e8e45e3f136a9ba7034644e
Kit Menke <cwmenke@gmail.com>
2020-10-23 11:24:25 -0500

    Add example tests for dropping duplicates and joining dataframes.

--- src/test/scala/com/kitmenke/spark/MyStreamingAppTests.scala
+++ src/test/scala/com/kitmenke/spark/MyStreamingAppTests.scala
@@ -2,6 +2,7 @@ package com.kitmenke.spark
 
 import com.holdenkarau.spark.testing.DataFrameSuiteBase
 import org.apache.spark.sql
+import org.apache.spark.sql.Row
 import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
 import org.scalatest.FunSuite
 
@@ -36,6 +37,49 @@ class MyStreamingAppTests extends FunSuite with DataFrameSuiteBase {
     // using the to_date spark sql function, convert the string value into a
     import org.apache.spark.sql.functions.to_date
     val result = df.select(to_date(df("dates"), "yyyyMMdd"))
+    result.printSchema()
+    result.show()
+  }
+
+  test("should drop duplicates") {
+    val schema = new StructType()
+      .add("row", StringType, nullable = false)
+      .add("code", StringType, nullable = false)
+    // create some sample data to run through our program
+    val rdd = sc.parallelize(Seq(
+      Row("row1", "XFH"),
+      Row("row2", "ABC"),
+      Row("row3", "XFH"),
+    ))
+    val df = sqlContext.createDataFrame(rdd, schema)
+    df.show()
+    val result = df.dropDuplicates("code")
+    result.printSchema()
+    result.show()
+  }
+
+  test("should join two dataframes") {
+    val schema1 = new StructType()
+      .add("ColumnA", StringType, nullable = false)
+      .add("ColumnB", StringType, nullable = false)
+    // create some sample data to run through our program
+    val rdd1 = sc.parallelize(Seq(
+      Row("a1", "b1"),
+      Row("a2", "b2"),
+      Row("a3", "b3"),
+    ))
+    val df1 = sqlContext.createDataFrame(rdd1, schema1)
+    val schema2 = new StructType()
+      .add("ColumnA", StringType, nullable = false)
+      .add("ColumnC", StringType, nullable = false)
+    val rdd2 = sc.parallelize(Seq(
+      Row("a1", "c1"),
+      Row("a2", "c2"),
+      Row("a4", "c3"),
+    ))
+    val df2 = sqlContext.createDataFrame(rdd2, schema2)
+
+    val result = df1.join(df2, df1("ColumnA") === df2("ColumnA"), "outer")
     result.printSchema()
     result.show()
   }
