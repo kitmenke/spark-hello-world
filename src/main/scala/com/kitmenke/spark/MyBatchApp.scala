f49870ec0a641064b883675ff98be9adf95ffe0c
Kit Menke <cwmenke@gmail.com>
2021-04-06 20:13:46 -0500

    Fix sorting issue with multiple cores.

--- src/main/scala/com/kitmenke/spark/MyBatchApp.scala
+++ src/main/scala/com/kitmenke/spark/MyBatchApp.scala
@@ -36,7 +36,7 @@ object MyBatchApp {
         .map(word => (word, 1))
         .reduceByKey(_ + _)
         .sortBy(t => t._2, ascending = false)
-      counts.foreach(println)
+      counts.collect().foreach(println)
     } catch {
       case e: Exception => logger.error(s"$jobName error in main", e)
     }
